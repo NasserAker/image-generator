016c692188c89d660f0c522ce957f56f72dc5358
Nasser Aker <nasseraker4@gmail.com>
2025-01-29 23:46:43 +0200

    test 3

--- app.py
+++ app.py
@@ -1,4 +1,37 @@
 import streamlit as st # type: ignore
+from together import Together # type: ignore
 
-st.title("Hello, Streamlit!!!!!!! 🚀")
-st.write("This is a simple Streamlit app.")
+st.title("AI Image Generator 🖼️")
+st.write("Enter a prompt to generate an AI-generated image.")
+
+# User input
+prompt = st.text_input("Enter your prompt:", "A futuristic city skyline at sunset")
+
+client = Together()
+
+if st.button("Generate Image"):
+    if prompt:
+        with st.spinner("Generating image... ⏳"):
+            try:
+                response = client.images.generate(
+                    prompt = prompt,
+                    model="black-forest-labs/FLUX.1-schnell-Free",
+                    steps=10,
+                    n=4
+                )
+                image_url = response.data[0].b64_json
+
+                # Display image
+                st.image(image_url, caption="Generated Image", use_column_width=True)
+
+                # # Download button
+                # img_data = requests.get(image_url).content
+                # img = Image.open(BytesIO(img_data))
+                # st.download_button(
+                #     label="Download Image",
+                #     data=img_data,
+                #     file_name="generated_image.png",
+                #     mime="image/png"
+                # )
+            except Exception as e:
+                st.error(f"Error: {e}")
--- image_model.py
+++ image_model.py
@@ -2,8 +2,13 @@ from together import Together # type: ignore
 
 
 class image_gen:
-    def __init__(self):
-        
-        
-
+    def get_img(prompt):
+        client = Together()
+        response = client.images.generate(
+            prompt,
+            model="black-forest-labs/FLUX.1-schnell-Free",
+            steps=10,
+            n=4
+        )
+        return(response.data[0].b64_json)
 
