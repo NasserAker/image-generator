00f8471b25a3381c751007efab95f24dafb68b21
Nasser Aker <nasseraker4@gmail.com>
2025-01-31 22:28:24 +0200

    test 12

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ if st.button("Generate Image"):
                 response = client.images.generate(
                     prompt=prompt,
                     model="black-forest-labs/FLUX.1-schnell-Free",
-                    steps=10,
+                    steps=4,
                     n=4
                 )
 
